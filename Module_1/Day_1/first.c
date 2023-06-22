3f3ecf05cb3c4ee8aa2ec71ac3794a5a7c2e5673
SnehalPawar22 <93332020+SnehalPawar22@users.noreply.github.com>
2023-06-22 21:56:59 +0530

    Add files via upload

---
+++ Module_1/Day_1/first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int biggest(int,int);
+
+int main() {
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    int result = biggest(num1, num2);
+    printf("The biggest number is: %d\n", result);
+    
+    return 0;
+}
+int biggest(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
